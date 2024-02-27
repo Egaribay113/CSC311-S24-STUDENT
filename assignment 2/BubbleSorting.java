886b6d10be7045faae69555474205768d18050e6
Egaribay113 <157911424+Egaribay113@users.noreply.github.com>
2024-02-26 23:05:08 -0800

    Add files via upload

---
+++ assignment 2/BubbleSorting.java
@@ -0,0 +1,69 @@
+//Eric Garibay
+//egaribay19@toromail.csudh.edu
+public class BubbleSorting {
+
+	private int[] size; 
+	int loopCounter; 
+	
+	BubbleSorting(int[]myArray){
+		
+		size = myArray; 
+	}
+	
+	
+	public int[] sortAsc(int[] myArray) {
+	    int loopCounter = 0;
+	    boolean swapped;
+	    //System.out.println("Ascending order");
+	    do {
+	        swapped = false; //Theres no swapping before the loop
+	        for (int i = 0; i < myArray.length - 1; i++) {
+	            if (myArray[i] > myArray[i + 1]) {
+	                int temp = myArray[i];
+	                myArray[i] = myArray[i + 1];
+	                myArray[i + 1] = temp;
+	                swapped = true; //true if it swaps element during loop iterations
+	            }
+	            loopCounter++;
+	        }
+	    } while (swapped); //Continues until no swaps occur
+	    System.out.println("Ascending order loops: "+ loopCounter);
+	    return myArray;
+	}
+		
+		
+	
+	
+	public int[] sortDesc(int[]myArray) {
+		
+		int loopCounter = 0;
+	    boolean swapped;
+	    do {
+	        swapped = false; //Theres no swapping before the loop
+	        for (int i = 0; i < myArray.length - 1; i++) {
+	            if (myArray[i] < myArray[i + 1]) { //swapped to less than
+	                int temp = myArray[i];
+	                myArray[i] = myArray[i + 1];
+	                myArray[i + 1] = temp;
+	                swapped = true; //true if it swaps element during loop iterations
+	            }
+	            loopCounter++;
+	        }
+	    } while (swapped); //Continues until no swaps occur
+	    System.out.println("Descending order loops: "+ loopCounter);
+	    return myArray;
+		    
+		    
+ 		
+		
+	}		
+		
+	
+	public int loopCycles(int loopCounter) {
+		
+		return loopCounter; 
+		
+	}
+
+}
+
